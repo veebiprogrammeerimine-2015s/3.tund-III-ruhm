c8ccb9abbacbcbb13e94647e570a54351c0669ef
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2015-09-16 13:17:02 +0300

    added input cleanup

--- page/login.php
+++ page/login.php
@@ -8,12 +8,19 @@
 	
 	$name_error = "";
 	
+	// muutujad ab väärtuste jaoks
+	$email = "";
+	$name = "";
+	
 	// kontrollime et keegi vajutas input nuppu
 	if($_SERVER["REQUEST_METHOD"] == "POST") {
 		
 		//echo "keegi vajutas nuppu";
 		
+		
+		// *******************
 		// vajutas login nuppu
+		// *******************
 		if(isset($_POST["login"])){ 
 			
 			echo "vajutas login nuppu!";
@@ -21,6 +28,10 @@
 			//kontrollin et e-post ei ole tühi
 			if ( empty($_POST["email"]) ) {
 				$email_error = "See väli on kohustuslik";
+			}else{
+				
+				$email = test_input($_POST["email"]);
+				
 			}
 			
 			//kontrollin, et parool ei ole tühi
@@ -37,26 +48,55 @@
 				}
 				
 			}
+			
+			// kontrollin et ei oleks ühtegi errorit
+			if($email_error == "" && $password_error ==""){
+				
+				echo "kontrollin sisselogimist ".$email." ja parool ";
+			}
+			
+			
 		
 		
-		
+		// **************************
 		// keegi vajutas create nuppu
+		// **************************
 		}elseif(isset($_POST["create"])){
 			
 			echo "vajutas create nuppu!";
 			
 			//valideerimine create user vormile
-			//kontrollin et e-post ei ole tühi
+			//kontrollin et nimi ei ole tühi
 			if ( empty($_POST["name"]) ) {
 				$name_error = "See väli on kohustuslik";
+			}else{
+				// kõik korras
+				// test_input eemaldab pahatahtlikud osad
+				$name = test_input($_POST["name"]);
+				
 			}
+			
+			if($name_error == ""){
+				echo "salvestan ab'i ".$name;
+			}
+			
+			
 			
 		}
 		
-		
-		
 	}
 	
+	
+	function test_input($data) {
+		// võtab ära tühikud, enterid, tabid
+		$data = trim($data);
+		// tagurpidi kaldkriipsud \ 
+		$data = stripslashes($data);
+		// teeb html'i tekstiks < läheb &lt;
+		$data = htmlspecialchars($data);
+		return $data;
+	}
+		
 ?>
 <?php 
 	$page_title = "Sisselogimise leht";
@@ -67,7 +107,7 @@
 	<h2>Log in</h2>
 		
 		<form action="login.php" method="post" >
-			<input name="email" type="email" placeholder="E-post"> <?php echo $email_error; ?><br><br>
+			<input name="email" type="email" placeholder="E-post" value="<?php echo $email; ?>"> <?php echo $email_error; ?><br><br>
 			<input name="password" type="password" placeholder="Parool"> <?php echo $password_error; ?> <br><br>
 			<input name="login" type="submit" value="Log in">
 		</form>
@@ -75,7 +115,7 @@
 	<h2>Create user</h2>
 		
 		<form action="login.php" method="post" >
-			<input name="name" type="text" placeholder="Eesnimi Perenimi">* <?php echo $name_error; ?><br><br>
+			<input name="name" type="text" placeholder="Eesnimi Perenimi" value="<?php echo $name; ?>">* <?php echo $name_error; ?><br><br>
 			<input name="create" type="submit" value="Create">
 		</form>
 	
